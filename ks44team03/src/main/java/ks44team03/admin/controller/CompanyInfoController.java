960da0fd48c9b0df3658974bf91e92388190ecb5
ejddi1805 <ejddi1805@naver.com>
2022-09-20 16:14:08 +0900

    [ks44team03] 템플릿 적용안되는거 수정
    author: 김덕현
    템플릿 적용안되는거 수정

--- ks44team03/src/main/java/ks44team03/admin/controller/CompanyInfoController.java
+++ ks44team03/src/main/java/ks44team03/admin/controller/CompanyInfoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/company")
+
 public class CompanyInfoController {
 	
 	
