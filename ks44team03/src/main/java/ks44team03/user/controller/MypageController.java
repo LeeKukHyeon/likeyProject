8b246d159b16cf910d0d2cbaac8b90c917d9523e
0504l <0504l@DESKTOP-ICECD9O.localdomain>
2022-09-22 07:58:30 +0900

    사이드바 추가 후 작업

--- ks44team03/src/main/java/ks44team03/user/controller/MypageController.java
+++ ks44team03/src/main/java/ks44team03/user/controller/MypageController.java
@@ -4,17 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-
 public class MypageController {
 	
-	
-	@PostMapping("/api/shipStepInfo")
-	public String test() {
-		return "api/ship_step_info_api";
+	@GetMapping("/mem_edit")
+	public String memEdit() {
+		return "myPage/mem_edit";
 	}
+	
+	@GetMapping("/myaddr_list")
+	public String myaddrList() {
+		return "myPage/myaddr_list";
+	}
+		
 	
 	@GetMapping("/mypage")
 	public String mypage() {
