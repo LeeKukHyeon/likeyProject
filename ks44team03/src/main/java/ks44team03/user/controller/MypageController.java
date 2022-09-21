ea70f51544bcad2cb0f06495c41c7b1d10884d15
0504lkh <0504lkh@naver.com>
2022-09-21 17:50:43 +0900

    수업중추가

--- ks44team03/src/main/java/ks44team03/user/controller/MypageController.java
+++ ks44team03/src/main/java/ks44team03/user/controller/MypageController.java
@@ -3,6 +3,7 @@ package ks44team03.user.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MypageController {
 	
 	
+	@PostMapping("/api/shipStepInfo")
+	public String test() {
+		return "api/ship_step_info_api";
+	}
 	
 	@GetMapping("/mypage")
 	public String mypage() {
