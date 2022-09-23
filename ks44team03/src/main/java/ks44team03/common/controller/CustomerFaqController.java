8bd082f36330f3f0788aa54fb3201db40990c7d4
pusuro <soung4195@naver.com>
2022-09-23 16:38:49 +0900

    [ks44team03] 공지사항 , 배송스케줄 등록 html 작성 author : 주성민 detail : 공지사항,배송스케줄 등록 html 작성 , 버튼누르면 th로 연결

--- ks44team03/src/main/java/ks44team03/common/controller/CustomerFaqController.java
+++ ks44team03/src/main/java/ks44team03/common/controller/CustomerFaqController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CustomerFaqController {
 
 	@GetMapping("/faqSearch")
-	public String faqSearch(Model model) {
+	public String viewFaq(Model model) {
 		return "/CustomerService/faqSearch";
 	}
 }
