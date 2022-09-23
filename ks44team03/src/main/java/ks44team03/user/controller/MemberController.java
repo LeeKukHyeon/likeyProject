2cc3f7c420776dcc1daa103b82fedd34e3972a47
hoeezza <hoeezza@gmail.com>
2022-09-23 13:44:27 +0900

    [ks44team03] 주문서파일등록 및 로그인 맵핑완 author : 문지혜 file : applicationFormRegister.html detail : 주문서 내용 등록 및 로그인관련 컨트롤러 맵핑완료

--- ks44team03/src/main/java/ks44team03/user/controller/MemberController.java
+++ ks44team03/src/main/java/ks44team03/user/controller/MemberController.java
@@ -1,5 +1,25 @@
 package ks44team03.user.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MemberController {
 
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "login/loginForm";
+	}
+	
+	@GetMapping("/findIdPw")
+	public String findIdPw() {
+		return "login/findIdPw";
+	}
+	
+	@GetMapping("/userForm")
+	public String userForm() {
+		return "registration/userForm";
+	}
+	
+	
 }
--- ks44team03/src/main/java/ks44team03/user/controller/OrderController.java
+++ ks44team03/src/main/java/ks44team03/user/controller/OrderController.java
@@ -1,5 +1,15 @@
 package ks44team03.user.controller;
 
-public class OrderController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class OrderController {
+	
+	@GetMapping("/applicationFormRegister")
+	public String applicationFormRegister() {
+		return "applicationForm/applicationFormRegister";
+	}
+
+	
 }
