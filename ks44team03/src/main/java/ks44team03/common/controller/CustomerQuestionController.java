0bdb56a21d7eac7b6361baa514f0bd186184809c
pusuro <soung4195@naver.com>
2022-09-26 10:27:51 +0900

    [ks44team03] 관리자 페이지 1:1문의 조회 추가 author : 주성민

--- ks44team03/src/main/java/ks44team03/common/controller/CustomerQuestionController.java
+++ ks44team03/src/main/java/ks44team03/common/controller/CustomerQuestionController.java
@@ -7,6 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CustomerQuestionController {
 
+	//1:1 문의 관리자 조회
+	@GetMapping("/personalQuestionSearchAdmin")
+	public String regResponseCustomerQuestion(Model model) {
+		return "/CustomerService/personalQuestionSearchAdmin";
+	}
+	
+	
+	// 1:1문의 등록
 	@GetMapping("/personalQuestionRegister")
 	public String regCustomerQuestion(Model model) {
 		return "CustomerService/personalQuestionRegister";
