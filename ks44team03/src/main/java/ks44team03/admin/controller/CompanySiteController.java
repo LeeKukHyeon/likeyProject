9b58dc63577ca73fc7c997c63d80c87369c3c9f8
0504lkh <0504lkh@naver.com>
2022-09-26 17:32:08 +0900

    [ks44team03] : 메인화면 구현
    author : 이국현
    
    file : main.html
    
    detail : 메인화면 사진 바꾸기
    
    ​

--- ks44team03/src/main/java/ks44team03/admin/controller/CompanySiteController.java
+++ ks44team03/src/main/java/ks44team03/admin/controller/CompanySiteController.java
@@ -7,6 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CompanySiteController {
 	
+	@GetMapping("test")
+	public String adminModifySite1(Model model) {
+		model.addAttribute("title", "가입한 쇼핑몰사이트 수정");
+		return "main";
+	}
+	
+	
+	@GetMapping("shoppingModify")
+	public String adminModifySite(Model model) {
+		model.addAttribute("title", "가입한 쇼핑몰사이트 수정");
+		return "procurement/shopping/shoppingModify";
+	}
+	
 	@GetMapping("shoppingRegister")
 	public String adminRegSite(Model model) {
 		model.addAttribute("title", "가입한 쇼핑몰사이트 등록");
