06884b739119a17eddc805ae87d042b4ee890350
pusuro <soung4195@naver.com>
2022-09-26 16:10:20 +0900

    [ks44team03] 고객센터 삭제 이외 author : 주성민 detail : 삭제 이외 관리자페이지,일반페이지 수정

--- ks44team03/src/main/java/ks44team03/common/controller/CustomerFaqController.java
+++ ks44team03/src/main/java/ks44team03/common/controller/CustomerFaqController.java
@@ -7,7 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CustomerFaqController {
 
-	// 1:1문의 조회
+	
+	//자주하는질문 등록
+	@GetMapping("/faqRegister")
+	public String faq1(Model model) {
+		return "/customerService/faqRegister";
+		
+	}
+	
+	// 자주하는질문 조회
 	@GetMapping("/faqSearch")
 	public String viewFaq(Model model) {
 		return "/customerService/faqSearch";
--- ks44team03/src/main/java/ks44team03/common/controller/CustomerNoticeController.java
+++ ks44team03/src/main/java/ks44team03/common/controller/CustomerNoticeController.java
@@ -7,16 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CustomerNoticeController {
 
+	
+	//공지사항 게시글
 	@GetMapping("/notice1")
 	public String notice1(Model model) {
 		return "/customerService/notice1";
 	}
 	
+	
+	//공지사항 등록
 	@GetMapping("/noticeRegister")
 	public String regNotice(Model model) {
 		return "/customerService/noticeRegister";
 	}
 	
+	
+	// 공지사항 조회
 	@GetMapping("/noticeSearch")
 	public String viewNotice(Model model) {
 		return "/customerService/noticeSearch";
