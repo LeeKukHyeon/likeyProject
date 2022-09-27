ed565baf03b5c6f340fce7099b7df672030ba0eb
0504lkh <0504lkh@naver.com>
2022-09-27 14:53:07 +0900

    [ks44team03] : Mypage 값 집어넣는중
    author : 이국현
    
    file : Mypage.html
    
    detail : sql 작성중

--- ks44team03/src/main/java/ks44team03/user/service/MypageService.java
+++ ks44team03/src/main/java/ks44team03/user/service/MypageService.java
@@ -1,5 +1,32 @@
 package ks44team03.user.service;
 
-public class MypageService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import ks44team03.admin.mapper.CompanyInfoMapper;
+import ks44team03.dto.CompanyInfo;
+import ks44team03.dto.UserInfo;
+import ks44team03.user.mapper.MypageMapper;
+
+
+
+@Service
+public class MypageService {
+	//의존성 주입
+		private MypageMapper mypageMapper;
+		
+		
+		public MypageService(MypageMapper mypageMapper) {
+			this.mypageMapper = mypageMapper;
+		}
+		
+		 public List<UserInfo> getUserList(String u_id){
+			  
+			 List<UserInfo> UserList = mypageMapper.getUserInfo(u_id);
+			 
+			 return UserList; 
+			 
+		}
+			 
 }
