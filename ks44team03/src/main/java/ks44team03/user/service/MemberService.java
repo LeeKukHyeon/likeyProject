6edb584cc215f3248666834c135ce8a40c3b987b
hoeezza <hoeezza@gmail.com>
2022-09-28 16:44:02 +0900

    ﻿[ks44team03] 멤버맵퍼수정중 author : 문지혜 detail : 멤버맵퍼수정중

--- ks44team03/src/main/java/ks44team03/user/service/MemberService.java
+++ ks44team03/src/main/java/ks44team03/user/service/MemberService.java
@@ -3,8 +3,12 @@ package ks44team03.user.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Transactional
+@Service
 public class MemberService {
+	/* private final */
 
 }
