3577a899466cd1d06ecf2efa206e49cf4d4d3e0b
wntjd <wntjd@DESKTOP-MCMG53G>
2022-09-29 23:52:53 +0900

    [ks44team03] 배송스케줄 등록 연결 author : 주성민

--- ks44team03/src/main/java/ks44team03/common/service/CustomerScheduleService.java
+++ ks44team03/src/main/java/ks44team03/common/service/CustomerScheduleService.java
@@ -2,9 +2,12 @@ package ks44team03.common.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ks44team03.common.controller.CustomerScheduleController;
 import ks44team03.common.mapper.CommonMapper;
 import ks44team03.common.mapper.CustomerScheduleMapper;
 import ks44team03.dto.ScheduleCenter;
@@ -13,6 +16,9 @@ import ks44team03.dto.ScheduleCenter;
 @Transactional
 public class CustomerScheduleService {
 
+	
+	private static final Logger log = LoggerFactory.getLogger(CustomerScheduleController.class);
+	
 	//의존성 주입
 	private CustomerScheduleMapper customerScheduleMapper;
 	private CommonMapper commonMapper;
@@ -28,15 +34,17 @@ public class CustomerScheduleService {
 		
 		String newScheduleCode = commonMapper.getCommonPkCode("delivery_schedule", "schedule_num_code");
 		scheduleCenter.setScheduleNumCode(newScheduleCode);
+		log.info(scheduleCenter + "::::::: 배송스케줄 추가 scheduleCenter");
 		
 		int result = customerScheduleMapper.regSchedule(scheduleCenter);
-
-	}
-	
+		
+		log.info(result  + "::::::: 배송스케줄 추가 result");
+	}	
+		
 	
 	
 	//배송스케줄 게시물 조회
-	public ScheduleCenter getScheduleRead(int scheduleNumCode) {
+	public ScheduleCenter getScheduleRead(String scheduleNumCode) {
 		
 		ScheduleCenter scheduleRead = customerScheduleMapper.getScheduleRead(scheduleNumCode);
 		
