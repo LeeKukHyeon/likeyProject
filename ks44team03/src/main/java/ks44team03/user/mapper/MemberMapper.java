370ed6b258a0a3d1a3cb0594a575f56d77674e13
hoeezza <hoeezza@gmail.com>
2022-09-29 13:30:59 +0900

    ss삭제

--- ks44team03/src/main/java/ks44team03/user/mapper/MemberMapper.java
+++ ks44team03/src/main/java/ks44team03/user/mapper/MemberMapper.java
@@ -1,13 +1,1 @@
 package ks44team03.user.mapper;
-/*
- * import org.apache.ibatis.annotations.Mapper;
- * 
- * import ks44team03.dto.SignUp;
- * 
- * @Mapper public interface MemberMapper { // 회원가입 처리
- * 
- * void signupAction(SignUp signUp);
- * 
- * 
- * }
- */
