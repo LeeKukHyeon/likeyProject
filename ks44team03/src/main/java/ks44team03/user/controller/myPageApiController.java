ff2f1774d0a0d382c1e8810cda6151af1f266531
0504lkh <0504lkh@naver.com>
2022-09-29 10:12:24 +0900

    [ks44team03] : mypage ajax 작업중
    author : 이국현
    
    file : mypageScreen.html
    
    detail : mypage 계속 작업중

--- ks44team03/src/main/java/ks44team03/user/controller/myPageApiController.java
+++ ks44team03/src/main/java/ks44team03/user/controller/myPageApiController.java
@@ -1,13 +1,30 @@
 package ks44team03.user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class myPageApiController {
 	
-	@PostMapping("api/ship_step_info_api")
-	public String abc() {
-		return "myPage/member/ship_step_info_api";
+	
+	@PostMapping("api/nodataListApi")
+	public String bcd(Model model) {
+		model.addAttribute("title", "title");
+		return "myPage/myPageApi/nodataList";
+	}
+	
+	
+	@PostMapping("shipStepInfo")
+	public String abc(@RequestParam(name="q_ship_step_type", defaultValue = "") String test) {
+		if (test.equals("noDataCnt")){
+			return "myPage/myPageApi/nodataApi";
+		}
+		else if (test.equals("Imsi")) {
+			return "myPage/myPageApi/tempApi";
+		}
+		
+		return null;
 	}
 }
