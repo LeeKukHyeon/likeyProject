15c22b698667fb066014f7b3e54dcc4e48065778
ejddi1805 <ejddi1805@naver.com>
2022-10-07 17:28:25 +0900

    [ks44team03] 커뮤니티 게시판 이용후기 구현
    author : 김덕현
    
    detail : 커뮤니티 게시판 이용후기 리스트, 등록 기능 구현

--- ks44team03/src/main/java/ks44team03/user/controller/CommunityController.java
+++ ks44team03/src/main/java/ks44team03/user/controller/CommunityController.java
@@ -1,9 +1,12 @@
 package ks44team03.user.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ks44team03.dto.Community;
 import ks44team03.user.service.CommunityService;
@@ -18,10 +21,26 @@ public class CommunityController {
 	}
 	// 이용후기 게시판 목록
 	@GetMapping("/reviewList")
-	public String reviewList(Model model) {
+	public String getReviewList(Model model) {
 		
+		List<Community> reviewList = communityService.getReviewList();
+		
+		System.out.println("reviewList :::: {}"+ reviewList);
+		model.addAttribute("reviewList", reviewList);
 		model.addAttribute("title", "이용후기");
+		
 		return "community/reviewList";
+	}
+	// 이용후기 상세정보
+	@GetMapping("/reviewInfo")
+	public String getReviewInfo(@RequestParam(name="communityNum")String communityNum,
+								Model model) {
+		
+		Community reviewInfo = communityService.getReviewInfo(communityNum);
+		
+		model.addAttribute("reviewInfo", reviewInfo);
+		model.addAttribute("title", "이용후기");
+		return "community/reviewInfo";
 	}
 	
 	// 이용후기 등록
@@ -32,6 +51,7 @@ public class CommunityController {
 		
 		return"redirect:/reviewList";
 	}
+	// 이용후기 등록
 	@GetMapping("/reviewRegister")
 	public String addReviewForm(Model model) {
 		
--- ks44team03/src/main/java/ks44team03/user/mapper/CommunityMapper.java
+++ ks44team03/src/main/java/ks44team03/user/mapper/CommunityMapper.java
@@ -1,5 +1,7 @@
 package ks44team03.user.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import ks44team03.dto.Community;
@@ -10,4 +12,9 @@ public interface CommunityMapper {
 	// 이용후기 등록
 	public int addReview(Community community);
 	
+	// 이용후기 목록
+	public List<Community> getReviewList();
+	
+	// 이용후기 상세정보
+	public Community getReviewInfo(String communityNum);
 }
