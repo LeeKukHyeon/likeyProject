4bcfc990508dcc927354efe05e3df114007d6ce8
pusuro <soung4195@naver.com>
2022-10-12 17:31:46 +0900

    [ks44team03] 쿠폰 맵퍼xml 수정중 author : 주성민

--- ks44team03/src/main/java/ks44team03/common/controller/CouponController.java
+++ ks44team03/src/main/java/ks44team03/common/controller/CouponController.java
@@ -1,11 +1,36 @@
 package ks44team03.common.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import ks44team03.common.service.CouponSerive;
+import ks44team03.dto.Coupon;
 
 @Controller
 public class CouponController {
 	
+	//의존성 주입
+	private CouponSerive couponSerive;
+	
+	public CouponController(CouponSerive couponSerive) {
+		this.couponSerive = couponSerive;
+	}
+	
+	
+	
+	//관리자 페이지 쿠폰 리스트 , 등록
+	@GetMapping("/couponAdmin")
+	public String couponAdmin(Model model) {
+		
+		List<Coupon> couponList = couponSerive.couponList();
+		
+		model.addAttribute("couponList", couponList);
+		
+		return "/couponManagementAdmin/couponAdmin";
+	}
 
 	@GetMapping("couponCheck")
 	public String couponList() {
